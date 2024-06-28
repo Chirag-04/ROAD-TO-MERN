804ae6dd3fb6b8e7ac2064fb51deaf0a05fa460b
Chirag-04 <95089015+Chirag-04@users.noreply.github.com>
2024-06-28 21:21:13 +0530

    initial commit

---
+++ JS/01_callback.js
@@ -0,0 +1,22 @@
+function add(a , b , fnToCall){
+    let res = a +b;
+    return res; 
+    fnToCall(res);
+}
+
+function display1(res){
+    console.log("sum in display1" , res);
+}
+
+
+function display2(res){
+    console.log("sum in display1" , res);
+}
+
+// const ans = add(1 , 2);
+// display1(ans);
+
+// but let say we are  only allowed to use single statement 
+
+const ans = add(1 , 2  , display1);
+// so i have passed a fn as an argument 
